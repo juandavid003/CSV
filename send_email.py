3d3ce68159f9a7bd67347801e523f038b6b2e0f2
Juan David <juan.ramirez.freire@udla.edu.ec>
2025-05-06 19:39:20 -0500

    Chnages

--- send_email.py
+++ send_email.py
@@ -8,27 +8,22 @@ def send_email(subject, body):
     receiver_email = "rjuandavid2002@gmail.com"
     password = "gdce uvja oaqp ojgr"
 
-    # Configurar el servidor SMTP
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
     server.login(sender_email, password)
 
-    # Construir el mensaje
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
     message["Subject"] = subject
 
-    # Agregar cuerpo del correo
     message.attach(MIMEText(body, "plain"))
 
-    # Enviar el correo
     server.sendmail(sender_email, receiver_email, message.as_string())
 
-    # Cerrar la conexión
     server.quit()
 
 if __name__ == "__main__":
-    subject = f"Notificación: {sys.argv[1]}"  # Job Name (success, fail, etc.)
+    subject = f"Notificación: {sys.argv[1]}"
     body = f"El estado del job es: {sys.argv[2]}\nVer detalles: {sys.argv[3]}"
     send_email(subject, body)
